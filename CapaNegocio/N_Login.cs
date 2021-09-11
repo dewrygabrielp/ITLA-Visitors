832fb47c730b164026a003083fbaa6375150a625
dewrygabrielp <87697718+dewrygabrielp@users.noreply.github.com>
2021-09-12 07:39:28 +0800

    Add files via upload

---
+++ CapaNegocio/N_Login.cs
@@ -0,0 +1,62 @@
+﻿using CapaDatos;
+using System.Data;
+using System;
+using System.Runtime.InteropServices;
+
+namespace CapaNegocio
+{
+    public class N_Login
+    {
+        //Metodos que enlazan con capa Datos
+        public static DataTable MostrarUsuarios(string usuario, string password)
+        {
+            D_Login Login = new D_Login();
+            Login.Usuario = usuario;
+            Login.Password = password;
+
+            return Login.MostrarUsuario(Login);
+
+        }
+        public static DataTable verLogin()
+        {
+            return new D_Login().VerLogin();
+
+        }
+        #region CRUD
+        public static string Insertar(string nombre, string apellido, DateTime fecha, string usuario, string password, string acceso)
+        {
+            D_Login Login = new D_Login();
+            Login.Nombre = nombre;
+            Login.Apellido = apellido;
+            Login.Fecha = fecha;
+            Login.Usuario = usuario;
+            Login.Password = password;
+            Login.Acceso = acceso;
+            return Login.Insertar(Login);
+        }
+        public static string Editar(int idusuario, string nombre, string apellido, DateTime fecha, string usuario, string password, string acceso)
+        {
+            D_Login Login = new D_Login();
+            Login.IdUsuario = idusuario;
+            Login.Nombre = nombre;
+            Login.Apellido = apellido;
+            Login.Fecha = fecha;
+            Login.Usuario = usuario;
+            Login.Password = password;
+            Login.Acceso = acceso;
+            return Login.Editar(Login);
+        }
+        public static string Eliminar(int idusuario)
+        {
+            D_Login Login = new D_Login();
+            Login.IdUsuario = idusuario;
+
+            return Login.Eliminar(Login);
+        }
+        public static DataTable MostrarAcceso()
+        {
+            return new D_Login().MostrarAcceso();
+        }
+        #endregion
+    }
+}
---
+++ CapaNegocio/N_Visitas.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using CapaDatos;
+
+namespace CapaNegocio
+{
+    public class N_Visitas
+    {
+        //Metodos
+        public static string Insertar(int idedifices, int idclassroom, string name, string lastname, int idcarrera, string reason, string mail,
+           DateTime entrytime, DateTime departuretime, byte[] photo)
+        {
+            D_Visitas visitas = new D_Visitas();
+
+            visitas.IdEdifice = idedifices;
+            visitas.IdClassroom = idclassroom;
+            visitas.Name = name;
+            visitas.LastName = lastname;
+            visitas.IdCarrera = idcarrera;
+            visitas.Reason = reason;
+            visitas.Mail = mail;
+            visitas.EntryTime = entrytime;
+            visitas.DepartureTime = departuretime;
+            visitas.Photo = photo;
+
+            return visitas.Insertar(visitas);
+        }
+        
+        public static string Editar(int idvisitas, int idedifices, int idclassroom, string name, string lastname, int idcarrera, string reason, string mail,
+           DateTime entrytime, DateTime departuretime, byte[] photo)
+        {
+            D_Visitas visitas = new D_Visitas();
+
+            visitas.IdVisites = idvisitas;
+            visitas.IdEdifice = idedifices;
+            visitas.IdClassroom = idclassroom;
+            visitas.Name = name;
+            visitas.LastName = lastname;
+            visitas.Reason = reason;
+            visitas.Mail = mail;
+            visitas.EntryTime = entrytime;
+            visitas.DepartureTime = departuretime;
+            visitas.Photo = photo;
+
+            return visitas.Editar(visitas);
+        }
+        public static string Eliminar(int idvisites)
+        {
+            D_Visitas visitas = new D_Visitas();
+
+            visitas.IdVisites = idvisites;
+    
+
+            return visitas.Eliminar(visitas);
+        }
+        public static DataTable MostrarVisitas()
+        {
+            return new D_Visitas().MostrarVisitas();
+        }
+        public  DataTable BuscarNombre(string textobuscar)
+        {
+            D_Visitas visitas = new D_Visitas();
+            visitas.Textobuscar = textobuscar;
+
+            return visitas.BuscarNombre(visitas);
+        }
+        public DataTable BuscarEdificio(int idedificio)
+        {
+            D_Visitas visitas = new D_Visitas();
+            visitas.IdEdifice = idedificio;
+            return visitas.BuscarEdificio(visitas);
+        }
+        public static DataTable MostrarEdificio()
+        {
+            return new D_Visitas().MostrarEdificio();
+        }
+        public static string InsertarEdificio(string edificio)
+        {
+            D_Visitas visitas = new D_Visitas();
+            visitas.Edificio = edificio;
+            return visitas.InsertarEdificio(visitas);
+        }
+        public static string EditarEdificio(int idedificio, string edificio)
+        {
+            D_Visitas visitas = new D_Visitas();
+            visitas.IdEdifice = idedificio;
+            visitas.Edificio = edificio;
+            return visitas.EditarEdificio(visitas);
+        }
+        public static string EliminarEdificio(int idedificio)
+        {
+            D_Visitas visitas = new D_Visitas();
+            visitas.IdEdifice = idedificio;
+            return visitas.EliminarEdificio(visitas);
+        }
+
+        public static DataTable MostrarAula()
+        {
+            return new D_Visitas().MostrarAula();
+        }
+        public static string InsertarAula(string aula)
+        {
+            D_Visitas visitas = new D_Visitas();
+            visitas.Aula = aula;
+            return visitas.InsertarAula(visitas);
+        }
+        public static string EditarAula(int idaula, string aula)
+        {
+            D_Visitas visitas = new D_Visitas();
+            visitas.IdClassroom = idaula;
+            visitas.Aula = aula;
+            return visitas.EditarAula(visitas);
+        }
+        public static string EliminarAula(int idaula)
+        {
+            D_Visitas visitas = new D_Visitas();
+            visitas.IdClassroom = idaula;
+            return visitas.EliminarAula(visitas);
+        }
+        public static DataTable MostrarCarreras()
+        {
+            return new D_Visitas().MostrarCarreras();
+        }
+        public string InsertarCarreras(string carrera)
+        {
+            D_Visitas visitas = new D_Visitas();
+            visitas.Carrera = carrera;
+            return visitas.InsertarCarreras(visitas);
+        }
+        public static string EditarCarreras(int idcarrera, string carrera)
+        {
+            D_Visitas visitas = new D_Visitas();
+            visitas.IdCarrera = idcarrera;
+            visitas.Carrera = carrera;
+            return visitas.EditarCarreras(visitas);
+        }
+        public static string EliminarCarreras(int idcarrera)
+        {
+            D_Visitas visitas = new D_Visitas();
+            visitas.IdCarrera = idcarrera;
+            return visitas.EliminarCarreras(visitas);
+        }
+    }
+}
